213e15124376a893bda7a8f65c2ef8ce2b93cd03
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-11-11 11:20:34 -0500

    Setup base content for games and studios

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gamestop' });
+  res.render('index');
 });
 
 router.get('/games', (req, res) => {
   res.render('games', { title: 'Games' });
+});
+
+router.get('/studios', (req, res) => {
+  res.render('studios', { title: 'Studios' });
+});
+
+router.get('/post', (req, res) => {
+  res.render('post');
 });
 
 module.exports = router;
