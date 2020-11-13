830db9c1528aedaa37ef25d111d27cda9b1bb3b3
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-11-13 13:54:19 -0500

    Add use of helmet (#7)
    * Resolve vulnerability in dependency
    
    * Install helmet to set headers appropriately
    
    * Place helmet at top

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const gamesRouter = require('./routes/games');
@@ -13,6 +14,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
