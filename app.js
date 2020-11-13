cdae978e609c58624f22c56b22b6e150277c31d0
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-11-13 12:31:12 -0500

    Place helmet at top

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet());
 
 app.use('/', indexRouter, gamesRouter, studioRouter);
 
