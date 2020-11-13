22ac926bc042bc69bc667ecffbf72feef304c7db
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-11-13 10:20:20 -0500

    Install helmet to set headers appropriately

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const gamesRouter = require('./routes/games');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', indexRouter, gamesRouter, studioRouter);
 
