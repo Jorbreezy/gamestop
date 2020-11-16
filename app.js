4a8f07acd9aa073313626372528a0aaba3f81f2b
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-11-16 12:31:18 -0500

    Install and config winston with morgan

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const winston = require('./config/winston');
 
 const indexRouter = require('./routes/index');
 const gamesRouter = require('./routes/games');
@@ -17,7 +18,7 @@ app.set('view engine', 'pug');
 
 app.use(helmet());
 
-app.use(logger('dev'));
+app.use(logger('combined', { stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -32,13 +33,17 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  if (err) {
+      // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    winston.error(`${err.status || 500} - ${err.message} - ${req.originalURL} - ${req.method} - ${req.ip} `)
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  } 
 });
 
 module.exports = app;
