88e4cfec999402a325c26e964bd92e51a8c1ac9a
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-11-17 15:00:50 -0500

    Add testing for studio

---
+++ cypress/integration/tests/studios.spec.js
@@ -0,0 +1,55 @@
+context('Studios', () => {
+  before(() => {
+    cy.task('db:rollback');
+    cy.task('db:latest');
+    cy.task('db:seed');
+  });
+
+  describe('Studio Data', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/studios');
+    });
+
+    it('Should have title studios', () => {
+      cy.get('.title')
+        .contains('Studios');
+    });
+
+    it('Should contain 3 divs', () => {
+      cy.get('.content')
+        .find('.tile')
+        .should('have.length', 3);
+    });
+  });
+
+  describe('New Studio', () => {
+    it('Should navigate to new studio', () => {
+      cy.visit('http://localhost:3000/studios/new');
+
+      cy.url().should('include', '/studios/new');
+    });
+
+    it('Should contain a form', () => {
+      cy.get('form');
+    });
+
+    it('Should pass in correct data', () => {
+      cy.get('form').within(($form) => {
+        cy.get('input[name="name"]').type('BluePoint Games');
+      });
+    });
+
+    it('Should Submit data', () => {
+      cy.get('input[type="submit"]').click();
+    });
+  });
+
+  describe('Delete Studio', () => {
+    it('Should delete a Studio', () => {
+      cy.get('.menu > form')
+        .eq(3)
+        .click();
+    });
+  });
+
+});
